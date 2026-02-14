34bfd30c858b0198aaeb6b4caafc2236b8c1307f
Sunanka <email@example.com>
2026-02-14 15:42:43 +0100

    first commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = "8363991333:AAGD3oZXvdh9wLgOTcT8eZUI44BvR_z0uz4"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Yunusa Cyber Bot is Online 24/7 🚀")
