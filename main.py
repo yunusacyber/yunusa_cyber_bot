4d0681edc44505d6ee4f37847a68327a39371ccb
yunusacyber <yunusaabdurrazzaq@gmail.com>
2026-02-14 21:31:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = "8363991333:AAGD3oZXvdh9wLgOTcT8eZUI44BvR_z0uz4"
+TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Yunusa Cyber Bot is Online 24/7 🚀")
 
 app = ApplicationBuilder().token(TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
 
-app.run_polling()
+PORT = int(os.environ.get("PORT", 10000))
+
+app.run_webhook(
+    listen="0.0.0.0",
+    port=PORT,
+    webhook_url="https://yunusa-cyber-bot.onrender.com"
+)
